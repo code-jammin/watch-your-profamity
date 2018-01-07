9c6a912fd78f68debb0cbc86f336d547f608f95c
CruftJammin <stephen.whitmey@gmail.com>
2018-01-07 08:46:04 +0000

    basic project structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # watch-your-profamity
+A simple profanity filter. Returns whether the submitted string contains profanity or not.
---
+++ src/main/java/com/creatingbugs/WatchYourProfamityApplication.java
@@ -0,0 +1,12 @@
+package com.creatingbugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchYourProfamityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchYourProfamityApplication.class, args);
+	}
+}
